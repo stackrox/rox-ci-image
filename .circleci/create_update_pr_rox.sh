e6fde41fb681f510b40649616f76bca9881f4020
Viswajith Venugopal <viswajithiii@gmail.com>
2020-09-23 14:45:56 -0700

    WIP

---
+++ .circleci/create_update_pr_rox.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+[[ -n "${GITHUB_TOKEN}" ]] || { echo >&2 "No GitHub token found"; exit 2; }
+
+usage() {
+  echo >&2 "Usage: $0 <branch_name>"
+  exit 2
+}
+
+branch_name="$1"
+
+[[ -n "$branch_name" ]] || usage
+
+pr_response_file="$(mktemp)"
+status_code_file="$(mktemp)"
+
+message="Hello,
+This is an automated PR created to bump the base image.
+It was created from ${CIRCLE_PULL_REQUEST}."
+
+curl -sS \
+  -w '%{http_status_code}' \
+	-o "$pr_response_file" \
+	-X POST \
+	-H "Authorization: token ${GITHUB_TOKEN}" \
+	'https://api.github.com/repos/stackrox/rox/pulls' \
+	-d"{
+	\"title\": \"Update rox-ci-image\",
+	\"body\": $(jq -sR <<<"$message"),
+	\"head\": \"${branch_name}\",
+	\"base\": \"master\"
+}" > "${status_code_file}"
+
+echo "Got status code: ${status_code_file}"
+echo "Got PR response: ${pr_response_file}"
