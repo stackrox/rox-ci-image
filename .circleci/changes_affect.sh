35c7ed73cf636152fc3cf887617bf58e28a8230b
Piotr Rygielski <prygiels@redhat.com>
2021-12-29 13:59:52 +0100

    Add conditional opening of PRs

---
+++ .circleci/changes_affect.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+set -uo pipefail
+
+all_changed_files() {
+  main_branch="$(git remote show origin | sed -n '/HEAD branch/s/.*: //p')"
+  [[ -n "${main_branch}" ]] || die "Failed to get main branch"
+
+  if ! diffbasesha="$(git merge-base HEAD "origin/${main_branch}")"; then
+    die "Failed to determine diffbasesha"
+  fi
+
+  IFS=$'\n' read -d '' -r -a all_changed_files < <(
+    {
+      git diff "$diffbasesha" --name-status . |
+      sed -n -E -e "s@^[AM][[:space:]]+|^R[^[:space:]]*[[:space:]]+[^[:space:]]+[[:space:]]+@@p" ;
+    } | sort | uniq -u ) || true
+
+  echo "${all_changed_files[*]}"
+}
+
+
+affects_all() {
+  [[ " $* " =~  images/static-contents ]]
+}
+
+affects_collector() {
+  [[ " $* " =~  images/Dockerfile.collector  ]]
+}
+
+affects_scanner() {
+  [[ " $* " =~  images/Dockerfile.rox  ]]
+}
+
+affects_stackrox() {
+  [[ " $* " =~  images/Dockerfile.rox  ]]
+}
+
+main() {
+  files="$(all_changed_files)"
+  if affects_all "${files}"; then
+    return 0
+  fi
+  case "${1}" in
+    rox|stackrox) affects_stackrox "${files}";;
+    collector) affects_collector "${files}";;
+    scanner) affects_scanner "${files}";;
+    *) false;;
+  esac
+}
+
+# Allow sourcing and running depending on a context
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+    main "$*"
+fi
